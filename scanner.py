dbaa6852ca52d60a3fadf608fc1e84f61b209e06
Oran Dennehy <24418943@studentmail.ul.ie>
2024-10-10 13:30:59 +0100

    scanner.py

--- scanner.py
+++ scanner.py
@@ -1,4 +1,11 @@
-import scapy.all as scapy
+import scapy.all as scapy # type: ignore
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
+    return options
 
 def scan(ip):
 
@@ -6,14 +13,22 @@ def scan(ip):
 
   broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 
-  arp_request_broadcast = broadcast/arp_request
+  arp_request_broadcast = broadcast / arp_request
 
   answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-  print("IP\t\t\tMAC Address\n-----------------------------------------")
+  clients_list = []
 
   for element in answered_list:
-    print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+    clients_list.append(client_dict)
+  return clients_list
 
+def print_result(results_list):
+  print("IP\t\t\tMAC Address\n-----------------------------------------")
+  for client in results_list:
+    print(client["ip"] + "\t\t" + client["mac"])
 
-scan("10.0.2.1/24") #Input your own ip address here, use ifconfig before running to find the ip address of your connected network .1/24 meaning to scan all from 1-254
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
